c1fac9ce5c9ddd0d3cd5110d02e47f0c6ec55a11
skybrawl111 <skybrawl111@gmail.com>
2026-06-19 23:22:31 +0400

    мой сайт 3

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -6,26 +6,29 @@ import a2 from './assets/img/dark_btn.png';
 import a3 from './assets/img/icon_btn.png';
 import a4 from './assets/img/icons8-search.svg';
 
+// Используем переменную для базового пути
+const BASE_PATH = process.env.PUBLIC_URL || '';
+
 const DATA = [
-  { id: 1, title: "CASCATE DI TIVOLI", year: 1761, artist: "JEAN-HONORE FRAGONARD", museum: "LOUVRE MUSEUM", img: "./src/assets/img/cascateditivoli.jpg" },
-  { id: 2, title: "PORTRAIT OF VINCENT VAN GOGH", year: 1886, artist: "VINCENT VAN GOGH", museum: "MUSEUM OF MODERN ART", img: "./src/assets/img/PortraitofVincentvanGogh.png" },
-  { id: 3, title: "UNEQUAL MARRIAGE", year: 1862, artist: "JEAN-HONORE FRAGONARD", museum: "LOUVRE MUSEUM", img: "./src/assets/img/Unequalmarriage.png" },
-  { id: 4, title: "THE HAPPY VIOLINIST", year: 1624, artist: "THOMAS GAINSBOROUGH", museum: "NATIONAL GALLERY", img: "./src/assets/img/ThehappyViolinist.png" },
-  { id: 5, title: "THE ARCADIAN", year: 1834, artist: "THOMAS GAINSBOROUGH", museum: "NATIONAL GALLERY", img: "./src/assets/img/TheArcadian.jpg" },
-  { id: 6, title: "GOLFO DI NAPOLI", year: 1845, artist: "THOMAS GAINSBOROUGH", museum: "NATIONAL GALLERY", img: "./src/assets/img/GolfodiNapoli.jpg" },
-  { id: 8, title: "MONA LISA", year: 1503, artist: "LEONARDO DA VINCI", museum: "LOUVRE MUSEUM", img: "./src/assets/img/Mona_Lisa,_by_Leonardo_da_Vinci,_from_C2RMF_retouched.jpg.webp" },
-  { id: 9, title: "GUERNICA", year: 1937, artist: "PABLO PICASSO", museum: "MUSEUM OF MODERN ART", img: "./src/assets/img/71Ion8xjswL._AC_UF1000,1000_QL80_.jpg" },
-  { id: 10, title: "THE KISS", year: 1907, artist: "GUSTAV KLIMT", museum: "BELVEDERE MUSEUM", img: "./src/assets/img/KL1267-1000x1000.jpg" },
-  { id: 11, title: "THE STARRY NIGHT", year: 1889, artist: "VINCENT VAN GOGH", museum: "MUSEUM OF MODERN ART", img: "./src/assets/img/The-Starry-Night-1200x630-1-979x514.jpg.webp" },
-  { id: 12, title: "LAS MENINAS", year: 1656, artist: "DIEGO VELAZQUEZ", museum: "PRADO MUSEUM", img: "./src/assets/img/Las_Meninas_(1656),_by_Velazquez.jpg" },
-  { id: 13, title: "THE SCREAM", year: 1893, artist: "EDVARD MUNCH", museum: "NATIONAL GALLERY", img: "./src/assets/img/1920px-The_Scream.jpg" },
-  { id: 14, title: "THE NIGHT WATCH", year: 1642, artist: "REMBRANDT", museum: "RIJKSMUSEUM", img: "./src/assets/img/La_ronda_de_noche,_por_Rembrandt_van_Rijn.jpg" },
-  { id: 15, title: "LIBERTY LEADING THE PEOPLE", year: 1830, artist: "EUGENE DELACROIX", museum: "LOUVRE MUSEUM", img: "./src/assets/img/La_Liberté_guidant_le_peuple_-_Eugène_Delacroix_-_Musée_du_Louvre_Peintures_RF_129_-_après_restauration_2024.jpg" },
-  { id: 16, title: "THE PERSISTENCE OF MEMORY", year: 1931, artist: "SALVADOR DALI", museum: "MUSEUM OF MODERN ART", img: "./src/assets/img/The-Persistence-of-Memory-canvas-collection-Salvador-1931.webp" },
-  { id: 17, title: "WANDERER ABOVE THE SEA OF FOG", year: 1818, artist: "CASPAR DAVID FRIEDRICH", museum: "KUNSTHALLE HAMBURG", img: "./src/assets/img/wandrear.jpg" },
-  { id: 18, title: "GIRL WITH A PEARL EARRING", year: 1665, artist: "JOHANNES VERMEER", museum: "MAURITSHUIS", img: "./src/assets/img/images.jpg" },
-  { id: 19, title: "IMPRESSION, SUNRISE", year: 1872, artist: "CLAUDE MONET", museum: "MUSEЕ MARMOTTAN MONET", img: "./src/assets/img/Monet_-_Impression,_Sunrise.jpg" },
-  { id: 20, title: "SPRINGTIME", year: 1873, artist: "PIERRE-AUGUSTE COT", museum: "METROPOLITAN MUSEUM", img: "./src/assets/img/PIERRE-AUGUSTE_COT_-_Primavera_(Museo_Metropolitano_de_Nueva_York,_1873._Óleo_sobre_lienzo,_213.4_x_127_cm).jpg" },
+  { id: 1, title: "CASCATE DI TIVOLI", year: 1761, artist: "JEAN-HONORE FRAGONARD", museum: "LOUVRE MUSEUM", img: `${BASE_PATH}/assets/img/cascateditivoli.jpg` },
+  { id: 2, title: "PORTRAIT OF VINCENT VAN GOGH", year: 1886, artist: "VINCENT VAN GOGH", museum: "MUSEUM OF MODERN ART", img: `${BASE_PATH}/assets/img/PortraitofVincentvanGogh.png` },
+  { id: 3, title: "UNEQUAL MARRIAGE", year: 1862, artist: "JEAN-HONORE FRAGONARD", museum: "LOUVRE MUSEUM", img: `${BASE_PATH}/assets/img/Unequalmarriage.png` },
+  { id: 4, title: "THE HAPPY VIOLINIST", year: 1624, artist: "THOMAS GAINSBOROUGH", museum: "NATIONAL GALLERY", img: `${BASE_PATH}/assets/img/ThehappyViolinist.png` },
+  { id: 5, title: "THE ARCADIAN", year: 1834, artist: "THOMAS GAINSBOROUGH", museum: "NATIONAL GALLERY", img: `${BASE_PATH}/assets/img/TheArcadian.jpg` },
+  { id: 6, title: "GOLFO DI NAPOLI", year: 1845, artist: "THOMAS GAINSBOROUGH", museum: "NATIONAL GALLERY", img: `${BASE_PATH}/assets/img/GolfodiNapoli.jpg` },
+  { id: 8, title: "MONA LISA", year: 1503, artist: "LEONARDO DA VINCI", museum: "LOUVRE MUSEUM", img: `${BASE_PATH}/assets/img/Mona_Lisa,_by_Leonardo_da_Vinci,_from_C2RMF_retouched.jpg.webp` },
+  { id: 9, title: "GUERNICA", year: 1937, artist: "PABLO PICASSO", museum: "MUSEUM OF MODERN ART", img: `${BASE_PATH}/assets/img/71Ion8xjswL._AC_UF1000,1000_QL80_.jpg` },
+  { id: 10, title: "THE KISS", year: 1907, artist: "GUSTAV KLIMT", museum: "BELVEDERE MUSEUM", img: `${BASE_PATH}/assets/img/KL1267-1000x1000.jpg` },
+  { id: 11, title: "THE STARRY NIGHT", year: 1889, artist: "VINCENT VAN GOGH", museum: "MUSEUM OF MODERN ART", img: `${BASE_PATH}/assets/img/The-Starry-Night-1200x630-1-979x514.jpg.webp` },
+  { id: 12, title: "LAS MENINAS", year: 1656, artist: "DIEGO VELAZQUEZ", museum: "PRADO MUSEUM", img: `${BASE_PATH}/assets/img/Las_Meninas_(1656),_by_Velazquez.jpg` },
+  { id: 13, title: "THE SCREAM", year: 1893, artist: "EDVARD MUNCH", museum: "NATIONAL GALLERY", img: `${BASE_PATH}/assets/img/1920px-The_Scream.jpg` },
+  { id: 14, title: "THE NIGHT WATCH", year: 1642, artist: "REMBRANDT", museum: "RIJKSMUSEUM", img: `${BASE_PATH}/assets/img/La_ronda_de_noche,_por_Rembrandt_van_Rijn.jpg` },
+  { id: 15, title: "LIBERTY LEADING THE PEOPLE", year: 1830, artist: "EUGENE DELACROIX", museum: "LOUVRE MUSEUM", img: `${BASE_PATH}/assets/img/La_Liberté_guidant_le_peuple_-_Eugène_Delacroix_-_Musée_du_Louvre_Peintures_RF_129_-_après_restauration_2024.jpg` },
+  { id: 16, title: "THE PERSISTENCE OF MEMORY", year: 1931, artist: "SALVADOR DALI", museum: "MUSEUM OF MODERN ART", img: `${BASE_PATH}/assets/img/The-Persistence-of-Memory-canvas-collection-Salvador-1931.webp` },
+  { id: 17, title: "WANDERER ABOVE THE SEA OF FOG", year: 1818, artist: "CASPAR DAVID FRIEDRICH", museum: "KUNSTHALLE HAMBURG", img: `${BASE_PATH}/assets/img/wandrear.jpg` },
+  { id: 18, title: "GIRL WITH A PEARL EARRING", year: 1665, artist: "JOHANNES VERMEER", museum: "MAURITSHUIS", img: `${BASE_PATH}/assets/img/images.jpg` },
+  { id: 19, title: "IMPRESSION, SUNRISE", year: 1872, artist: "CLAUDE MONET", museum: "MUSEЕ MARMOTTAN MONET", img: `${BASE_PATH}/assets/img/Monet_-_Impression,_Sunrise.jpg` },
+  { id: 20, title: "SPRINGTIME", year: 1873, artist: "PIERRE-AUGUSTE COT", museum: "METROPOLITAN MUSEUM", img: `${BASE_PATH}/assets/img/PIERRE-AUGUSTE_COT_-_Primavera_(Museo_Metropolitano_de_Nueva_York,_1873._Óleo_sobre_lienzo,_213.4_x_127_cm).jpg` },
 ];
 
 const LIMIT = 6;
